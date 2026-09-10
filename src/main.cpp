880852d7cbf7890b8a1940a5855ad4331097e84d
Roman Shynkarenko <shinkarom@gmail.com>
2026-09-10 11:42:05 +0200

    Add glfw, glad and miniaudio

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <miniaudio.h>
 
 int main(int argc, char *argv[])
 {
 	std::cout<<"Hello World!";
+	glfwInit();
+	GLFWwindow *window = glfwCreateWindow(320, 240, "ps1emu", nullptr, nullptr);
+	glfwMakeContextCurrent(window);
+	
+	gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+	
+	glEnable(GL_DEPTH_TEST);
+	
+	
+	while(!glfwWindowShouldClose(window)){
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	
+	glfwTerminate();
 	return 0;
 }

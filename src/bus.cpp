568011da39ab64080e16c9cd0b7714c8c0ce2cf1
Roman Shynkarenko <shinkarom@gmail.com>
2026-09-17 13:34:07 +0200

    Add basic Bus read/write callabcks

--- src/bus.cpp
+++ src/bus.cpp
@@ -2,7 +2,7 @@
 #include <cstring>
 #include <cstdio>
 
-Bus::Bus(): RAM(RAMSize), BIOS(BIOSSize){
+Bus::Bus(): RAM(RAMSize), BIOS(BIOSSize), Scratchpad(ScratchpadSize){
 	
 }
 
@@ -14,4 +14,55 @@ bool Bus::loadBIOS(unsigned char* fileData, int fileSize) {
 	auto loadedSize = fileSize > BIOSSize ? BIOSSize : fileSize;
 	std::memcpy(BIOS.data(), fileData, loadedSize);
 	return true;
+}
+
+uint32_t Bus::resolveAddress(uint32_t address){
+	return address & 0x1FFFFFFF;
+}
+
+uint8_t Bus::read8(uint32_t address) {
+	auto resolved = resolveAddress(address);
+	if(resolved>=0&&resolved<RAMSize){
+		return RAM[resolved];
+	} else if (resolved>=0x1FC00000&&resolved<0x1FC00000+BIOSSize){
+		return BIOS[resolved-0x1FC00000];
+	} else if (resolved>=0x1F800000&&resolved<0x1F800000+ScratchpadSize) {
+		return Scratchpad[resolved-0x1F800000];
+	}
+	return 0;
+}
+
+uint16_t Bus::read16(uint32_t address){
+	auto lo=read8(address);
+	auto hi=read8(address+1);
+	return (hi<<8)|lo;
+}
+
+uint32_t Bus::read32(uint32_t address){
+	auto lo=read16(address);
+	auto hi=read16(address+2);
+	return (static_cast<uint32_t>(hi)<<16)|lo;
+}
+
+void Bus::write8(uint32_t address, uint8_t value){
+	auto resolved = resolveAddress(address);
+	if(resolved>=0&&resolved<RAMSize){
+		RAM[resolved] = value;
+	} else if (resolved>=0x1F800000&&resolved<0x1F800000+ScratchpadSize) {
+		Scratchpad[resolved-0x1F800000]= value;
+	}
+}
+
+void Bus::write16(uint32_t address, uint16_t value){
+	auto lo=value&0xFF;
+	auto hi=(value>>8)&0xFF;
+	write8(address, lo);
+	write8(address+1, hi);
+}
+
+void Bus::write32(uint32_t address, uint32_t value){
+	auto lo=value&0xFFFF;
+	auto hi=(value>>16)&0xFFFF;
+	write16(address, lo);
+	write16(address+2, hi);
 }
--- src/bus.h
+++ src/bus.h
@@ -4,13 +4,22 @@
 
 constexpr auto RAMSize = 2*1024*1024;
 constexpr auto BIOSSize = 512*1024;
+constexpr auto ScratchpadSize = 1024;
 
 class Bus {
 	public:
 		Bus();
 		~Bus();
 		bool loadBIOS(unsigned char* fileData, int fileSize);
+		uint8_t read8(uint32_t address);
+		uint16_t read16(uint32_t address);
+		uint32_t read32(uint32_t address);
+		void write8(uint32_t address, uint8_t value);
+		void write16(uint32_t address, uint16_t value);
+		void write32(uint32_t address, uint32_t value);
 	private:
 		std::vector<uint8_t> RAM;
 		std::vector<uint8_t> BIOS;
+		std::vector<uint8_t> Scratchpad;
+		uint32_t resolveAddress(uint32_t address);
 };

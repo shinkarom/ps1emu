c0b5b0d8cf324c20a4a4affb5088003f34f131f4
Roman Shynkarenko <shinkarom@gmail.com>
2026-09-20 18:00:14 +0200

    Add SW, ORI, SLL, ADDIU

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -24,14 +24,43 @@ void CPU::step() {
 	pc+=4;
 	nextPC=pc+4;
 	auto opcode=(instr>>26)&0b111111;
+    auto rs     = (instr >> 21) & 0b11111;
+    auto rt     = (instr >> 16) & 0b11111;
+    auto rd     = (instr >> 11) & 0b11111;
+	uint32_t imm    = instr & 0xFFFF;
+    int32_t  imm_se = (int16_t)(instr & 0xFFFF);
 	switch(opcode){
+		case 0x00:{ //SPECIAL
+			auto opcode2 = instr & 0b111111;
+			switch(opcode2){
+				case 0x00:{ // SLL
+					auto sa = (instr>>6)&0b11111;
+					regs[rd] = regs[rt]<<sa; 
+					break;
+				}
+				default:
+					throw std::runtime_error(std::format("Unknown special opcode {:02X} at {:08X}", opcode2, pc-4));
+			}
+			break;
+		}
+		case 0x09:{ // ADDIU
+			regs[rt] = regs[rs] + imm_se;
+			break;
+		}
+		case 0x0D:{ //ORI
+			regs[rt] = regs[rs] | imm;
+			break;
+		}
 		case 0x0F:{ //LUI
-		auto imm = instr & 0xFFFF;
-		auto rt =(instr>>16)&0b11111;
-		regs[rt] = imm<<16;
-		break;}
+			regs[rt] = imm<<16;
+			break;}
+		case 0x2B:{ //SW
+			auto addr = regs[rs] + imm_se;
+			bus->write32(addr, regs[rt]);
+			break;
+		}
 		default:
-			throw std::runtime_error(std::format("Unknown opcode {:02X} at {:08X}", opcode, pc-4 	));
+			throw std::runtime_error(std::format("Unknown opcode {:02X} at {:08X}", opcode, pc-4));
 	}
 	regs[0]=0;
 }

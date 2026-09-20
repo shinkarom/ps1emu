89b24ac060d6ae2635240d2c35efb5073b3319d4
Roman Shynkarenko <shinkarom@gmail.com>
2026-09-20 19:02:51 +0200

    Add BNE, ADDI

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -18,6 +18,7 @@ void CPU::reset() {
 	lo=0;
 	pc=0xBFC00000;
 	nextPC=pc+4;
+	instrCount = 0;
 }
 
 void CPU::step() {
@@ -44,7 +45,7 @@ void CPU::step() {
 					break;
 				}
 				default:
-					throw std::runtime_error(std::format("Unknown special opcode {:02X} at {:08X}", opcode2, pc-4));
+					throw std::runtime_error(std::format("Unknown special opcode {:02X} at {:08X}, {:d} executed", opcode2, pc-4, instrCount));
 			}
 			break;
 		}
@@ -53,24 +54,22 @@ void CPU::step() {
 			nextPC = (pc&0xF0000000)|addr;
 			break;
 		}
+		case 0x05:{ // BNE
+			auto addr = pc + (imm_se*4);
+			if(regs[rs]!=regs[rt]){
+				nextPC = addr;
+			}
+			break;
+		}
+		case 0x08:{ // ADDI
+			regs[rt] = regs[rs] + imm_se;
+			// spec says should do overflow exception if overflow happens instead of mdifying rt
+			break;
+		}
 		case 0x09:{ // ADDIU
 			regs[rt] = regs[rs] + imm_se;
 			break;
 		}
-		case 0x10:{ // COP0
-			switch(rs){
-				case 0x04:
-					regsCOP0[rd] = regs[rt];
-					break;
-				default:
-					throw std::runtime_error(std::format("Unknown COP0 opcode {:02X} at {:08X}", rs, pc-4));
-			}
-			break;
-		}
-		case 0x11: //COP1
-			throw std::runtime_error("COP1 unusable");
-		case 0x13: //COP3
-			throw std::runtime_error("COP3 unusable");
 		case 0x0D:{ //ORI
 			regs[rt] = regs[rs] | imm;
 			break;
@@ -78,6 +77,20 @@ void CPU::step() {
 		case 0x0F:{ //LUI
 			regs[rt] = imm<<16;
 			break;}
+		case 0x10:{ // COP0
+			switch(rs){
+				case 0x04:
+					regsCOP0[rd] = regs[rt];
+					break;
+				default:
+					throw std::runtime_error(std::format("Unknown COP0 opcode {:02X} at {:08X}, {:d} executed", rs, pc-4, instrCount));
+			}
+			break;
+		}
+		case 0x11: //COP1
+			throw std::runtime_error("COP1 unusable");
+		case 0x13: //COP3
+			throw std::runtime_error("COP3 unusable");
 		case 0x25:{ // LHU
 			auto addr = regs[rs] + imm_se;
 			regs[rt] = bus->read16(addr);
@@ -89,7 +102,8 @@ void CPU::step() {
 			break;
 		}
 		default:
-			throw std::runtime_error(std::format("Unknown opcode {:02X} at {:08X}", opcode, pc-4));
+			throw std::runtime_error(std::format("Unknown opcode {:02X} at {:08X}, {:d} executed", opcode, pc-4, instrCount));
 	}
 	regs[0]=0;
+	instrCount++;
 }
--- src/cpu.h
+++ src/cpu.h
@@ -15,4 +15,5 @@ class CPU {
 		uint32_t regsCOP0[32];
 		uint32_t hi, lo;
 		uint32_t pc, nextPC;
+		int instrCount = 0;
 };
